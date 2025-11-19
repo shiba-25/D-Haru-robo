c284ea9bc8e4e1d82016eb9f8a995ad97ef13676
shiba-25 <chestnut9165@gmail.com>
2025-11-19 17:31:14 +0900

    add minus

--- src/MOTOR.hpp
+++ src/MOTOR.hpp
@@ -22,14 +22,16 @@ class Mech
     const int motor_power = 5000;
     const int motor_stop = 0;
 
-
 public:
     int motor_pwm[3] = {0};
-    void chiikawa_mech_turn() { motor_pwm[0] = motor_power; };
+    void chiikawa_mech_turn_plus() { motor_pwm[0] = motor_power; };
+    void chiikawa_mech_turn_minus() { motor_pwm[0] = -motor_power; };
     void chiikawa_mech_turn_stop() { motor_pwm[0] = motor_stop; };
-    void chiikawa_mech_updn() { motor_pwm[1] = motor_power; };
+    void chiikawa_mech_up() { motor_pwm[1] = motor_power; };
+    void chiikawa_mech_down() { motor_pwm[1] = -motor_power; };
     void chiikawa_mech_updn_stop() { motor_pwm[1] = motor_stop; };
-    void kandai_mech() { motor_pwm[2] = motor_power; };
+    void kandai_mech_plus() { motor_pwm[2] = motor_power; };
+    void kandai_mech_minus() { motor_pwm[2] = -motor_power; };
     void kandai_mech_stop() { motor_pwm[2] = motor_stop; };
 };
 
